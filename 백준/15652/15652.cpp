00422eee04f550b803ad4bf71611e6edd6a284ad
YooCobb <yhj010597@naver.com>
2019-11-05 16:29:15 +0900

    N 과 M (4)

---
+++ 백준/15652/15652.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#define MAX 8
+using namespace std;
+
+int arr[MAX];
+int m,n;
+
+void dfs(int a, int b){
+	if(b == n) {
+		for(int i=0;i<n;i++)
+			printf("%d ",arr[i]);
+		printf("\n");
+	}
+	
+	else
+		for(int i=a;i<m;i++){
+			arr[b] = i+1;
+			dfs(i,b+1);	
+		}
+}
+
+int main(){
+	cin >> m >> n;
+	
+	dfs(0,0);
+}
