104b82baf89ecb20bb823ae17229034ae9ec9cf4
YooCobb <yhj010597@naver.com>
2020-01-01 10:49:22 +0900

    최소힙

---
+++ 백준/1927/1927.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	priority_queue<int, vector<int>, greater<int>> arr;
+	
+	int n,m;
+	cin >> n;
+	
+	for(int i=0;i<n;i++){
+		cin >> m;
+		if(!m && arr.empty()){
+			cout << "0\n";
+		}
+		else if(!m){
+			cout << arr.top() << "\n";
+			arr.pop();
+		}
+		else{
+			arr.push(m);
+		}
+	}
+}
