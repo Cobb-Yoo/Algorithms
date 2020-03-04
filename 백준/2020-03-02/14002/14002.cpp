6f0149616838a4a7f61c9c30808b90aeac0318bc
YooCobb <yhj010597@naver.com>
2020-03-04 11:26:07 +0900

    add code

--- 백준/2020-03-02/14002/14002.cpp
+++ 백준/2020-03-02/14002/14002.cpp
@@ -1,1 +1,62 @@
-
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+ 
+int n;
+int arr[1001];
+int d[1001];//±æÀÌ distance 
+int v[1001];// °ª? 
+vector<int>answer;
+ 
+void go(int index) {
+    if (index == -1) {
+ 
+        return;
+    }
+    answer.push_back(arr[index]);
+    go(v[index]);
+}
+ 
+ 
+int main() { 
+    cin >> n;
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+ 
+    for (int i = 0; i < n; i++) {
+        v[i] = -1;
+        d[i] = 1;
+        for (int j = 0; j < i; j++) {
+            if (arr[i] > arr[j] && d[i] < d[j] + 1) {
+                d[i] = d[j] + 1;
+                v[i] = j;
+            }
+        }
+    }
+ 
+    int ans = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        ans = max(ans, d[i]);
+    }
+ 
+    cout << ans << endl;
+ 
+    for (int i = 0; i < n; i++) {
+        if (d[i]==ans){
+            answer.push_back(arr[i]);
+            go(v[i]);
+            break;
+        }
+    }
+ 
+    sort(answer.begin(), answer.end());
+    for (int i = 0; i < answer.size(); i++) {
+        cout << answer[i] << " ";
+    }
+ 
+}
