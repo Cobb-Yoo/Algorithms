f42784a773fe75a948182531f3205784d468d28b
YooCobb <yhj010597@naver.com>
2020-04-30 00:32:14 +0900

    9251 LCS

---
+++ 백준/9251 LCS/9251 LCS.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	string s1;
+	string s2;
+	
+	cin >> s1;
+	cin >> s2;
+	
+	int len1 = s1.size();
+	int len2 = s2.size();
+	
+	int dp[len1+1][len2+1];
+	memset(dp,0,sizeof(dp));
+	
+	for(int i=1;i<=len1;i++){
+		for(int j=1;j<=len2;j++){
+			if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
+			else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+		}
+	}
+	
+	cout << dp[len1][len2] << endl;
+}
