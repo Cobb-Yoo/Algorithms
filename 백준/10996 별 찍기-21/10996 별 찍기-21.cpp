9963d60279b8f2fc7bfc3c6999799c8ccc35d484
YooCobb <yhj010597@naver.com>
2020-04-30 15:28:16 +0900

    10996 별 찍기 -21

---
+++ 백준/10996 별 찍기-21/10996 별 찍기-21.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	
+	int len = n*2;
+	bool env = 1;
+	
+	for(int i=0;i<len;i++){
+		i&1 ? env=1 : env =0;
+		for(int j=0;j<n;j++){
+			if(env) cout << "*";
+			else cout << " ";
+			env ^= 1;
+		}
+		cout << endl;
+	}
+}
