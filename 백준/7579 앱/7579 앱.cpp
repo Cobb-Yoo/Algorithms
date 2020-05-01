bdfe82c5586eeb1043c551e7863fe87e99bf1574
YooCobb <yhj010597@naver.com>
2020-05-01 19:54:35 +0900

    7579 앱 코드수정

--- 백준/7579 앱/7579 앱.cpp
+++ 백준/7579 앱/7579 앱.cpp
@@ -22,7 +22,7 @@ int main(){
 	for(int i=1;i<n;i++){
 		for(int j=0;j<=10000;j++){
 			if(j-disabled[i] >= 0) dp[i][j] = max(dp[i][j], dp[i-1][j-disabled[i]]+usingM[i]);
-			else dp[i][j] = max(dp[i][j],dp[i-1][j]);
+			dp[i][j] = max(dp[i][j],dp[i-1][j]);
 			
 			if(dp[i][j] >= m) answer = min(answer, j);
 		}
