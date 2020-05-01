1a0835ef574e91c18983e27d7991a4dba6745904
YooCobb <yhj010597@naver.com>
2020-05-01 20:14:40 +0900

    11726 2xn 타일링

---
+++ 백준/11726 2xn 타일링/11726 2xn 타일링.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 1000;
+
+int arr[MAX+1];
+
+void init(){
+	arr[1] = 1;
+	arr[2] = 2;
+	
+	for(int i=3;i<=MAX;i++){
+		arr[i] = arr[i-1] + arr[i-2];
+	}
+}
+
+int main(){
+	int n;
+	cin >> n;
+	init();
+	
+	cout << arr[n] << endl;
+}
