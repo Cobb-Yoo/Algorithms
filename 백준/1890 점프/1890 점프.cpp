fc11fbd3ea930a8dc446a35438e5e4651b1dda17
YooCobb <yhj970105@gmail.com>
2020-05-07 02:23:19 +0900

    1890 점프

--- 백준/1890 점프/1890 점프.cpp
+++ 백준/1890 점프/1890 점프.cpp
@@ -1,39 +1,33 @@
 #include <iostream>
-#include <queue>
+#include <cstring>
+#define ll long long
 using namespace std;
 
+int n;
+ll dp[101][101];
+int arr[101][101];
+
+ll path(int x, int y){
+	if(x == n-1 && y == n-1) return 1;
+	if(dp[x][y] != -1) return dp[x][y];
+	dp[x][y] = 0;
+	
+	if(x != n-1 && x+arr[x][y] < n) dp[x][y] += path(x+arr[x][y],y);
+	if(y != n-1 && y+arr[x][y] < n) dp[x][y] += path(x,y+arr[x][y]);
+
+	return dp[x][y];
+}
+
 int main(){
-	int n,cnt=0;
 	cin >> n;
 	
-	int arr[n+1][n+1];
+	memset(dp,-1,sizeof(dp));
 	
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
 			cin >> arr[i][j];
 		}
 	}
 	
-	queue<pair<int,int>> q;
-	q.push({1,1});
-	
-	while(!q.empty()){
-		int x = q.front().first;
-		int y = q.front().second;
-		int tmp = arr[x][y];
-		q.pop();
-		
-		if(tmp+x <= n){
-			if(x+tmp == n && y == n) cnt++;
-			else q.push({x+tmp,y});	
-		} 
-		if(tmp+y <= n){
-			if(x == n && y+tmp == n) cnt++;
-			else q.push({x,y+tmp});	
-		} 
-		
-		
-	}
-	
-	cout << cnt << endl;
+	cout << path(0,0) << endl;
 }
