5e7d7471018532744558d09879b0469701c113c8
YooCobb <yhj970105@gmail.com>
2020-05-14 19:35:32 +0900

    temp

---
+++ 백준/6359 만취한 상법/6359 만취한 상법.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int MAX = 100;
+
+bool b[MAX+1];
+int dp[MAX+1];
+
+int main(){
+	int T;
+	cin >> T;
+	
+	
+	while(T--){
+		int n;
+		cin >> n;
+		
+		memset(b,1,sizeof(b));
+		dp[1] = 1;
+		
+		for(int i=2;i<=n;i++){
+			for(int j=i;j<=n;j+=i){
+				b[j] = !b[j];
+			}
+			
+			if(b[i]) dp[i] = dp[i-1]+1;
+			else dp[i] = dp[i-1];
+		}
+		
+		int cnt=0;
+		for(int i=1;i<=n;i++) if(b[i]) cnt++;
+		
+		cout << cnt << endl;
+	}
+}
