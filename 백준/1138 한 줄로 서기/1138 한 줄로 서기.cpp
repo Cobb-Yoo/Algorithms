73ba6db7201a30b13950cd032e87130dee46edd9
YooCobb <yhj010597@naver.com>
2020-06-17 05:45:09 +0900

    1138 한 줄로 서기

---
+++ 백준/1138 한 줄로 서기/1138 한 줄로 서기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	int left;
+	int arr[10]={0};
+	
+	for(int i=0;i<n;i++){
+		cin >> left;
+		
+		for(int j=0;j<n;j++){
+			if(!left && arr[j] == 0){
+				arr[j] = i+1;
+				break;
+			}
+			else if(arr[j] == 0) left--;
+		}
+	}
+	
+	for(int i=0;i<n;i++) cout << arr[i] << " ";
+}
