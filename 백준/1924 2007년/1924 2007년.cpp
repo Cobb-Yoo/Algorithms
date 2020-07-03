76738f3b8e61c063c51b4f35872ae3043a1a82ec
YooCobb <yhj010597@naver.com>
2020-07-03 11:40:12 +0900

    1924 2007년

--- 백준/1924 2007년/1924 2007년.cpp
+++ 백준/1924 2007년/1924 2007년.cpp
@@ -2,18 +2,17 @@
 #include <string>
 using namespace std;
 
-int marr[] = {31,29,31,30,31,30,31,31,30,31,30,31};
+int marr[] = {31,28,31,30,31,30,31,31,30,31,30,31};
 string darr[] = { "SUN","MON", "TUE", "WED", "THU", "FRI", "SAT"};
 
 int main(){
 	int x,y;
 	cin >> x >> y;
 	
+	
 	int cal = 0;
 	
 	for(int i=0;i<x-1;i++) cal += marr[i];
-	cout << "cal : " << cal << endl;
 	cal += y;
-	cout << "cal : " << cal << endl;
 	cout << darr[cal%7] << endl;
 }
