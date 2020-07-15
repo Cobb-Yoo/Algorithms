d6de0bdf833f7197f01c93a3b0b16ef64e028f6d
YooCobb <yhj010597@naver.com>
2020-07-15 22:39:00 +0900

    10988 팰린드롬인지 확인하기

---
+++ 백준/10988 팰린드롬인지 확인하기/10988 팰린드롬인지 확인하기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string s;
+	cin >> s;
+	
+	int len = s.size();
+	
+	for(int i=0;i<=len/2;i++){
+		if(s[i] != s[len-i-1]){
+			cout << 0 << endl;
+			return 0;
+		}
+	}
+	
+	cout << 1 << endl;
+}
