58d8e02e6d3a96feae05eccda72a83215d5aac2c
YooCobb <yhj010597@naver.com>
2020-07-20 20:52:21 +0900

    11004 k번째 수

---
+++ 백준/11004 k번째 수/11004 k번째 수.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n,k;
+	cin >> n >> k;
+	
+	vector<long> v(n);
+	
+	for(int i=0;i<n;i++) cin >> v[i];
+	
+	sort(v.begin(), v.end());
+	
+	cout << v[k-1] << endl;
+}
