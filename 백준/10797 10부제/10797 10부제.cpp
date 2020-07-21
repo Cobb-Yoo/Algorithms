adb0d8837e93b224c9d399a6a777e57e98b7dda6
YooCobb <yhj010597@naver.com>
2020-07-21 16:08:41 +0900

    10797 10부제

---
+++ 백준/10797 10부제/10797 10부제.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int answer = 0;
+	
+	int n;
+	cin >> n;
+	
+	for(int i=0;i<5;i++){
+		int tmp;
+		cin >> tmp;
+		
+		if(tmp == n) answer++;
+	}
+	
+	cout << answer << endl;
+}
