2d701fa55c93adbbbe248d8d05a86526512177a8
YooCobb <yhj010597@naver.com>
2020-07-21 23:02:40 +0900

    iot1@iot1 MINGW64 ~/Desktop/Algorithms (master) $

---
+++ 백준/18870 좌표 압축/18870 좌표 압축.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<pair<int, int> > v(n);
+    vector<int> answer(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i].first;
+        v[i].second = i;
+    }
+
+    sort(v.begin(), v.end());
+
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        int idx = v[i].second;
+        int tmp = cnt;
+
+        answer[idx] = tmp;
+        cnt++;
+
+        while (i < n && v[i].first == v[i + 1].first) {
+            ++i;
+            idx = v[i].second;
+            answer[idx] = tmp;
+        }
+    }
+
+    for (auto a : answer) cout << a << " ";
+
+}
