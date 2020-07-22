8a362db5987baee920e61c327fef98288b921a86
YooCobb <yhj010597@naver.com>
2020-07-23 03:05:24 +0900

    11003 최솟값 찾기

---
+++ 백준/11003 최솟값 찾기/11003 최솟값 찾기.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+const int MAX = 5000000;
+
+int arr[MAX];
+deque<pair<int,int>> dq;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n,l;
+	cin >> n >> l;
+	
+	for(int i=0;i<n;i++) cin >> arr[i];
+	
+	for(int i=0;i<n;i++){
+		if(!dq.empty() && dq.front().second <= i-l)
+			dq.pop_front();
+		
+		while(!dq.empty() && dq.back().first > arr[i])
+			dq.pop_back();
+		
+		dq.push_back({arr[i],i});
+		cout << dq.front().first << " ";
+	}
+}
