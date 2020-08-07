17f0cf0ceeaf468c8807d61fa25beb430aad2698
YooCobb <yhj010597@naver.com>
2020-08-07 12:24:59 +0900

    2042 구간 합 구하기

--- 백준/2042 구간 합 구하기/2042 구간 합 구하기.cpp
+++ 백준/2042 구간 합 구하기/2042 구간 합 구하기.cpp
@@ -1,63 +1,55 @@
 #include <iostream>
-#include <vector>
+#define ll long long
 using namespace std;
 
 const int MAX = 1000000;
+ll arr[MAX+1];
+ll tree[MAX*3];
 
-int n,m,k;
-long long arr[MAX];
-long long tree[MAX*4];
-
-long long init(int start, int end, int node){
-	if(start==end) return tree[node] = arr[start];
-	int mid = (start+end)/2;
+void update(int node, int start, int end, int idx, int dif){
+	if(start<=idx && idx<=end) tree[node] += dif;
+	else return;
 	
-	return tree[node] = init(start,mid,node*2) + init(mid+1,end,node*2+1);
-}
-
-long long sum(int start, int end, int node, int left, int right){
-	if(left > end || right < start) return 0;
-	if(left<=start && end<=right) return tree[node];
+	if(start == end) return;
 	
 	int mid = (start+end)/2;
-	return sum(start,mid,node*2,left,right)+sum(mid+1,end,node*2+1,left,right);
+	update(node*2,start,mid,idx,dif);
+	update(node*2+1,mid+1,end,idx,dif);
 }
 
-void update(int start, int end, int node, int index, int dif){
-	if(index < start || index > end) return;
-	tree[node]+=dif;
-	
-	if(start==end) return;
+ll getSum(int left, int right, int node, int start, int end){
+	if(left<= start && end <= right) return tree[node];
+	if(right < start || end < left) return 0;	
 	
 	int mid = (start+end)/2;
-	update(start,mid,node*2,index,dif);
-	update(mid+1,end,node*2+1,index,dif);
+	return getSum(left, right, node*2, start, mid)+getSum(left, right, node*2+1, mid+1, end);
 }
 
+ll init(int node, int start, int end){
+	if(start == end) return tree[node] = arr[start];
+	
+	int mid = (start+end)/2;
+	tree[node] = init(node*2, start, mid)+init(node*2+1,mid+1,end);
+	return tree[node];
+}
 int main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	
+	int n,m,k;
 	cin >> n >> m >> k;
-	for(int i=0;i<n;i++) {
-		cin >> arr[i];
-	}
 	
-	init(0,n-1,1);
+	for(int i=1;i<=n;i++) cin >> arr[i];
+	init(1,1,n);
 	
 	for(int i=0;i<m+k;i++){
 		int a,b,c;
 		cin >> a >> b >> c;
 		
-		
 		if(a == 1){
-			int x = c-arr[b-1];
-			update(0,n-1,1,b-1,x);
+			int x = c - arr[b];
+			arr[b] = c;
+			update(1,1,n,b,x);
 		}
 		else{
-			cout << sum(0,n-1,1,b-1,c-1) << endl;
+			cout << getSum(b,c,1,1,n) << endl;
 		}
-		//a가 1이면 b번째 수를 c로 바꾼다.
-		//a가 2이면 b부터 c까지의 수의 합을 구한다. 
 	}
 }
