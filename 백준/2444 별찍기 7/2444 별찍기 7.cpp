408c64d306ef20d871f95989b4ac5de2336d066f
YooCobb <yhj010597@naver.com>
2020-08-11 15:55:56 +0900

    2444 별찍기 7

---
+++ 백준/2444 별찍기 7/2444 별찍기 7.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n; cin >> n;
+	
+	int len = 1;
+	for(int i=n-1;i>0;i--){
+		for(int j=0;j<i;j++){
+			cout << " ";
+		}
+		
+		for(int j=0;j<len;j++){
+			cout << "*";
+		}
+		len += 2;
+		cout << endl;
+	}
+	
+	len = 2*n-1;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<i;j++){
+			cout << " ";
+		}
+		
+		for(int j=0;j<len;j++){
+			cout << "*";
+		}
+		len -= 2;
+		cout << endl;
+	}
+}
