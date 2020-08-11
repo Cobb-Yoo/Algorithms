5743d9bb2127d965f83f352918628107c51275e5
YooCobb <yhj010597@naver.com>
2020-08-12 03:06:05 +0900

    6588 골드바흐의 추측

---
+++ 백준/6588 골드바흐의 추측/6588 골드바흐의 추측.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+bool arr[1000001];
+vector<int> v;
+
+void check(int n){
+	for(int i=0;;i++){
+		if(arr[n-v[i]]){
+			cout << n << " = " << v[i] << " + " << n-v[i] << endl;
+			return;
+		}
+	}
+	
+	cout << "Goldbach's conjecture is wrong." << endl;
+}
+
+void init(){
+	memset(arr,1,sizeof(arr));
+	
+	for(int i=2;i*i<=1000000;i++){
+		if(arr[i]){
+			for(int j=i+i;j<=1000000;j+=i){
+				arr[j] = 0;
+			}
+		}
+	}
+	
+	for(int i=2;i<=1000000;i++){
+		if(arr[i]) v.push_back(i);
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	init();
+	
+	int n;
+	
+	while(cin >> n && n != 0){
+		check(n);
+	}
+}
