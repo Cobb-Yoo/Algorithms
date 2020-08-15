a56c104792049b11f9344ae378e184407751aafb
YooCobb <yhj010597@naver.com>
2020-08-15 14:43:51 +0900

    9372 상근이의 여행

---
+++ 백준/9372 상근이의 여행/9372 상근이의 여행.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+
+int main(){
+	int t; cin >> t;
+	
+	while(t--){
+		cin >> n >> m;
+		
+		for(int i=0;i<m;i++){
+			int a,b;
+			cin >> a >> b;
+		}
+		cout << n-1 << endl; 
+	}
+}
