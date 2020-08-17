8b625ebb418e68a94e24573dddb11a5372598d47
YooCobb <yhj010597@naver.com>
2020-08-17 10:01:15 +0900

    10971 외판원 순회 2

---
+++ 백준/10971 외판원 순회 2/10971 외판원 순회 2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int map[10][10];
+
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n; cin >> n;
+	int answer = 987654321;
+	
+	for(int i=0;i<n;i++) {
+		for(int j=0;j<n;j++){
+			cin >> map[i][j];
+		}
+	}
+	
+	vector<int> option(n);
+	for(int i=0;i<n;i++) option[i] = i;
+	
+	do{
+		int sum = 0;
+		bool flag = 1;
+		
+		for(int i=0;i<n-1;i++){
+			if(map[option[i]][option[i+1]] != 0){
+				sum += map[option[i]][option[i+1]];
+			}
+			else {
+				flag = 0;
+				break;
+			}
+		}
+		
+		if(flag && map[option[n-1]][option[0]] != 0){
+			sum += map[option[n-1]][option[0]];
+			answer = min(answer, sum);
+		}
+	}while(next_permutation(option.begin(), option.end()));
+	
+	cout << answer << endl;
+}
