230ca064c60b00967946a9c0b8e49aca9994c169
YooCobb <yhj010597@naver.com>
2020-08-19 17:26:05 +0900

    11721 열 개씩 끊어 출력하기

---
+++ 백준/11721 열 개씩 끊어 출력하기/11721 열 개씩 끊어 출력하기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string s;
+	cin >> s;
+	
+	int len = s.size();
+	
+	for(int i=0;i<len;i++){
+		if(i!=0 && i%10 == 0) {
+			cout << endl << s[i];
+		}
+		else cout << s[i];
+	}
+}
