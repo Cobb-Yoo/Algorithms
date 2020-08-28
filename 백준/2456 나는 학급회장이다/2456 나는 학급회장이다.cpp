a7087be36f0327d10831a7aa858c15933e753370
YooCobb <yhj010597@naver.com>
2020-08-29 01:54:10 +0900

    2456 나는 학급회장이다

--- 백준/2456 나는 학급회장이다/2456 나는 학급회장이다.cpp
+++ 백준/2456 나는 학급회장이다/2456 나는 학급회장이다.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
+bool cmp(vector<int> a, vector<int> b){
+	if(a[0] == b[0]){
+		if(a[1] == b[1]){
+			if(a[2] == b[2]) return a[3] > b[3];
+			return a[2] > b[2];
+		}
+		return a[1] > b[1];
+	}
+	return a[0] > b[0];
+}
+
 int main(){
-	int arr[4]={0};
 	int n; cin >> n;
 	
-	while(n--){
-		int tmp;
-		for(int i=3;i>=1;i--) {
-			cin >> tmp;
-			arr[tmp] += i;
+	vector<vector<int>> v;
+	v.push_back({-1,-1,-1,-1,-1});
+	for(int i=1;i<4;i++) v.push_back({0,0,0,0,i});
+	
+	for(int i=0;i<n;i++){
+		for(int j=1;j<=3;j++){
+			int num; cin >> num;
+			
+			v[j][num]++;
+			v[j][0] += num;
 		}
 	}
-	sort(arr,arr+4);
-	for(int i=0;i<4;i++) cout << arr[i] << " ";
-	cout << endl;
-	if(arr[3] == arr[2]) cout << 0 << endl;
-	else cout << arr[3] << endl;
+	
+	sort(v.begin(), v.end(), cmp);
+	
+	if(v[0][0] == v[1][0] && v[0][1] == v[1][1] && v[0][2] == v[1][2] && v[0][3] == v[1][3]) cout << 0 << " " << v[0][0] << endl;
+	else cout << v[0][4] << " " << v[0][0] << endl;
+	
+	
 }
