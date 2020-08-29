eb1f0fae2357ef4ee24b8c3365143cc5c761a204
YooCobb <yhj010597@naver.com>
2020-08-30 04:34:54 +0900

    1159 농구경기

---
+++ 백준/1159 농구경기/1159 농구경기.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int arr[26];
+
+int main(){
+	int n; cin >> n;
+	
+	for(int i=0;i<n;i++){
+		string s;
+		cin >> s;
+		
+		arr[s[0]-'a']++;
+	}
+	
+	bool flag = 1; 
+	for(int i=0;i<26;i++){
+		if(arr[i] >= 5) {
+			cout << (char)(i+'a');
+			bool = 0;
+		}
+	}
+	
+	if(flag) cout << "PREDAJA\n";
+}
