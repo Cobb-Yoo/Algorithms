b9226bc1ab4b9e9f8d621d8e31e95f053214c208
YooCobb <yhj010597@naver.com>
2020-08-30 18:59:23 +0900

    13460 구슬 탈출 2

--- 백준/13460 구슬 탈출 2/13460 구슬 탈출 2.cpp
+++ 백준/13460 구슬 탈출 2/13460 구슬 탈출 2.cpp
@@ -1,28 +1,94 @@
+#include<queue>
 #include <iostream>
 using namespace std;
-
-int n,m;
-char map[10][10];
-pair<int,int> red;
-pair<int,int> blue;
-pair<int,int> hole;
-
-
-int main(){
-	cin >> n >> m;
-	
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			cin >> map[i][j];
-			
-			if(map[i][j] == 'R') red = {i,j};
-			else if(map[i][j] == 'B') blue = {i,j};
-			else if(map[i][j] == 'O') hole = {i,j};
-		}
-	}
-	
-	//4가지 움직임에 따라서 red와 blue의 좌표만을 확인해서 움직임의 우선순위를 판단한다.
-	//우선순위가 먼저인 공이 끝까지 움직인다.
-	//그 다음 우선순위의 공이 움직인다.
-	//queue로 움직인 후의 값을 저장하여 bfs를 준비한다. 
+char map[10][11];
+int m, n;
+ 
+struct COD {
+    int br, bc;
+    int rr, rc;
+};
+ 
+COD st;
+bool visit[10][10][10][10];
+ 
+int dr[4] = { -1,1,0,0 };
+int dc[4] = { 0,0,-1,1 };
+ 
+int bfs() {
+    queue<COD> que;
+    visit[st.br][st.bc][st.rr][st.rc] = true;
+    int time = 0;
+    bool escape = false;
+ 
+    que.push(st);
+ 
+    while (que.size()) {
+        int cnt = que.size();
+ 
+        while (cnt--) {
+            COD now = que.front();
+            que.pop();
+ 
+            if (map[now.rr][now.rc] == 'O') { escape = true; break; }
+ 
+            for (int d = 0; d < 4; d++) {
+                bool out[2] = { false,false };
+                int t[2] = {};
+                int p[2][2] = { { now.br, now.bc }, {now.rr, now.rc} };
+ 
+                for (int i = 0; i < 2; i++) {
+                    while(map[p[i][0]+dr[d]][p[i][1]+dc[d]] !='#'){
+                        t[i]++;
+                        p[i][0] = p[i][0] + dr[d];
+                        p[i][1] = p[i][1] + dc[d];
+                        if (map[p[i][0]][p[i][1]] == 'O') break;
+                    }
+                    out[i] = map[p[i][0]][p[i][1]] == 'O';
+                }
+ 
+                if (out[0]) continue;
+                
+                if (p[0][0] == p[1][0] && p[0][1] == p[1][1]) {
+                    if (t[0] > t[1]) p[0][0] -= dr[d], p[0][1] -= dc[d];
+                    else p[1][0] -= dr[d], p[1][1] -= dc[d];
+                }
+                
+                if (visit[p[0][0]][p[0][1]][p[1][0]][p[1][1]]) continue;
+                visit[p[0][0]][p[0][1]][p[1][0]][p[1][1]] = true;
+                
+                COD next = { p[0][0], p[0][1], p[1][0], p[1][1] };
+                que.push(next);
+            }
+        }
+        if (escape) break;
+        time++;
+ 
+        if (time > 10) break;
+    }
+    return escape ? time : -1;
+}
+ 
+int main() {
+    cin >> n >> m;
+ 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> map[i][j];
+            if (map[i][j] == 'R') {
+                st.rr = i;
+                st.rc = j;
+                map[i][j] = '.';
+            }
+            else if (map[i][j] == 'B') {
+                st.br = i;
+                st.bc = j;
+                map[i][j] = '.';
+            }
+        }
+    }
+ 
+    cout << bfs();
+ 
+    return 0;
 }
