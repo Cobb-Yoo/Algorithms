aee5863edb43d1a2601d67254c560fc06ecf482d
YooCobb <yhj010597@naver.com>
2020-10-22 17:52:55 +0900

    10992 별찍기 17

--- 백준/10992 별찍기 17/10992 별찍기 17.cpp
+++ 백준/10992 별찍기 17/10992 별찍기 17.cpp
@@ -2,16 +2,23 @@
 using namespace std;
 
 int main(){
-	int n; cin >> n;
+	int n; cin >>n;
 	
-	for(int i=1;i<n;i++) cout << " ";
-	cout << "*\n";
-	
-	for(int i=2;i<n;i++){
-		for(int j=i;j<n;j++) cout << " ";
-		cout << "*";
-		for(int j=2;j<i*2-1;j++) cout << " ";
-		cout << "*\n";
+	for(int i=0;i<n;i++){
+		if(i==0){
+			for(int j=1;j<n-i;j++){
+				cout << " ";
+			}
+			cout << "*\n";
+		}
+		else if (i==n-1){
+			for(int j=0;j<2*n-1;j++)cout << "*";
+		}
+		else{
+			for(int j=1;j<n-i;j++) cout << " ";
+			cout << "*";
+			for(int j=0;j<i*2-1;j++) cout << " ";
+			cout << "*\n";
+		}
 	}
-	for(int i=1;i<=n*2-1;i++) cout << "*";
 }
