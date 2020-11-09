1f5498c4d6e5ffb8a92feaa25841f11c2e0ca0f7
YooCobb <yhj010597@naver.com>
2020-11-09 16:50:24 +0900

    5597 과제 안 내신 분

---
+++ 백준/5597 과제 안 내신 분/5597 과제 안 내신 분.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+bool arr[31];
+
+int main(){
+	int n;
+	for(int i=0;i<28;i++){
+		cin >> n;
+		
+		arr[n] = 1;
+	}
+	
+	for(int i=1;i<=30;i++){
+		if(!arr[i]) cout << i << endl;
+	}
+}
