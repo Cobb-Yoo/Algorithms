973b44e4e18d45156896a42c38674251d120e685
YooCobb <yhj010597@naver.com>
2020-12-10 01:15:02 +0900

    2357 최솟값과 최댓값

--- 백준/2357 최솟값과 최댓값/2357 최솟값과 최댓값.cpp
+++ 백준/2357 최솟값과 최댓값/2357 최솟값과 최댓값.cpp
@@ -15,8 +15,8 @@ ll init_maxTree(vector<ll> &maxTree, int node, int start, int end){
 		return maxTree[node] = v[start];
 	}
 	else{
-		int mid = (start+end)/2;
-		return maxTree[node] = max(init_maxTree(maxTree, node*2, start, mid), init_maxTree(maxTree, node*2+1, mid+1, end));
+		int mid = (start+end) >> 1;
+		return maxTree[node] = max(init_maxTree(maxTree, node<<1, start, mid), init_maxTree(maxTree, (node<<1)+1, mid+1, end));
 	}
 }
 
@@ -28,7 +28,7 @@ ll get_max(vector<ll> &maxTree, int node, int start, int end, int left, int righ
 		return maxTree[node];
 	}
 	
-	int mid = (start+end)/2;
+	int mid = (start+end) >> 1;
 	return max(get_max(maxTree, node*2, start, mid, left, right), get_max(maxTree, node*2+1, mid+1, end, left,right));
 }
 
@@ -37,8 +37,8 @@ ll init_minTree(vector<ll> &minTree, int node, int start, int end){
 		return minTree[node] = v[start];
 	}
 	else{
-		int mid = (start+end)/2;
-		return minTree[node] = min(init_minTree(minTree, node*2, start, mid), init_minTree(minTree, node*2+1, mid+1, end));
+		int mid = (start+end) >> 1;
+		return minTree[node] = min(init_minTree(minTree, node<< 1, start, mid), init_minTree(minTree, (node<< 1)+1, mid+1, end));
 	}
 }
 
@@ -50,21 +50,21 @@ ll get_min(vector<ll> &minTree, int node, int start, int end, int left, int righ
 		return minTree[node];
 	}
 	
-	int mid = (start+end)/2;
-	return min(get_min(minTree, node*2, start, mid, left, right), get_min(minTree, node*2+1, mid+1, end, left,right));
+	int mid = (start+end) >> 1;
+	return min(get_min(minTree, node << 1, start, mid, left, right), get_min(minTree, (node << 1)+1, mid+1, end, left,right));
 }
 
 int main(){	
 	ios_base::sync_with_stdio(0);
 	cin.tie();
 	
-	cin >> n >> m;
+	scanf("%d %d", &n, &m);
 	int h = (int) ceil(log2(n));
     int tree_size = (1 << (h + 1));
     
-    
+    int tmp;
 	for(int i=0;i<n;i++){
-		int tmp; cin >> tmp;
+		scanf("%d", &tmp);
 		v.push_back(tmp);
 	}
 	
@@ -77,8 +77,8 @@ int main(){
 	
 	int a,b;
 	for(int i=0;i<m;i++){
-		cin >> a >> b;
+		scanf("%d %d", &a, &b);
 		
-		cout << get_min(minTree, 1, 0, n-1, a-1 , b-1) << " " << get_max(maxTree, 1, 0, n-1, a-1 , b-1) << "\n";
+		printf("%lld %lld\n",get_min(minTree, 1, 0, n-1, a-1 , b-1), get_max(maxTree, 1, 0, n-1, a-1 , b-1));
 	}
 }
