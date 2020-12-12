c5f4fb315b101ec6570c3cad6b62a528def811c4
YooCobb <yhj010597@naver.com>
2020-12-13 04:49:22 +0900

    17219 비밀번호 찾기

--- 백준/17219 비밀번호 찾기/17219 비밀번호 찾기.cpp
+++ 백준/17219 비밀번호 찾기/17219 비밀번호 찾기.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <map>
 #include <string>
-#include <vector>
-#include <algorithm>
 using namespace std;
+
+const int MAX = 100000+1;
+int n,m;
+map<string, string> maP;
 
 int main(){
 	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 	
-	
-	int n,m;
 	cin >> n >> m;
+	for(int i=0;i<n;i++){
+		string tmp1,tmp2;
+		cin >> tmp1 >> tmp2;
+		maP.insert({tmp1,tmp2});
+	}
+	
+	for(int i=0;i<m;i++){
+		string tmp; cin >> tmp;
+		cout << maP[tmp] << "\n";
+	}
 }
