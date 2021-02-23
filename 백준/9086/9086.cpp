fa5c02b688242860b3d081cab8db57b5be86b5f3
YooCobb <yhj010597@naver.com>
2021-02-24 01:39:14 +0900

    9086

---
+++ 백준/9086/9086.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t; cin >> t;
+	string str;
+	
+	
+	while(t--){
+		cin >> str;
+		
+		cout << str[0] << str[str.size()-1] << endl;
+	}
+}
