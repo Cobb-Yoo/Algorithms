17a01a5b4407cae1025097a8414feccf3f4ce81e
YooCobb <yhj010597@naver.com>
2021-10-01 01:05:10 +0900

    18352 수정필요함

--- BOJ/18352/18352.cpp
+++ BOJ/18352/18352.cpp
@@ -1,12 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,m,k,x;
+int n; // 도시의 개수 
+int m; // 도로의 개수 
+int k; // 거리 정보  
+int x; // 출발 도시의 번호 
+
+const int Inf = 987654321;
+const int Max = 1000000;
+vector<pair<int,int>> graph[Max];
+priority_queue<int> answer;
+
+void solve(){
+	queue<pair<int,int>> q;
+	q.push({x,0});
+	
+	while(!q.empty()){
+		int now = q.front().first;
+		int nowCost = q.front().first;
+		q.pop();
+		
+		int len = graph[now].size();
+		
+		for(int i=0;i<len;i++){
+			neighbor = graph[now][i].first;
+			neighborCost = graph[now][i].second;
+			
+			if(neighborCost > nowCost+1){
+				neighborCost = nowCost+1;
+			}
+			
+			q.push({neighbor, neighborCost});
+		}
+	}
+}
 
 int main(){
 	cin >> n >> m >> k >> x;
+	int a,b;
 	
 	for(int i=0;i<m;i++){
+		cin >> a >> b;
 		
+		graph[a].push(b);
 	}
+	
+	solve();
 }
