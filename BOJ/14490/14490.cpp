d6e203e58def31e84a136b00cb8af907c6b9c475
Cobb-Yoo <yhj010597@naver.com>
2021-10-08 01:00:54 +0900

    14490

--- BOJ/14490/14490.cpp
+++ BOJ/14490/14490.cpp
@@ -2,11 +2,8 @@
 using namespace std;
 
 int gcd(int a, int b){
-    if(b == 0){
-        return a;
-    }else{
-        return gcd(b, a%b);
-    }
+    if(b == 0) return a;
+	else return gcd(b, a%b);
 }
 
 int main(){
